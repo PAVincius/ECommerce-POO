3e6cf51bbb1e6f2be64a9e2f9959ffeb8797072c
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-08-09 19:50:11 -0300

    :test_tube: test: missed import fixed
    ShouldTestInvalidCpf assertThrows.

--- backend/ecommerce/src/test/java/com/poo/ecommerce/CpfTests.java
+++ backend/ecommerce/src/test/java/com/poo/ecommerce/CpfTests.java
@@ -1,7 +1,7 @@
 package com.poo.ecommerce;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
