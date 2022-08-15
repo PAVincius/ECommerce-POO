7ceff2df5d1a3ad5b1f147dcc2e038deae814a9c
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-08-15 10:31:31 -0300

    :sparkles: feat: Item Class

---
+++ backend/ecommerce/src/main/java/com/poo/ecommerce/Item.java
@@ -0,0 +1,11 @@
+package com.poo.ecommerce;
+
+public class Item {
+    public Item(int i, String string, String string2, int j) {
+    }
+    String idItem;
+    String category;
+    String description;
+    String price;
+    
+}
