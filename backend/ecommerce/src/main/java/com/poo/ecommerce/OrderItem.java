df205a68d78bac67d70b688079d43f6577a82d1d
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-08-15 10:33:23 -0300

    :sparkles: feat: OrdemItem Class

---
+++ backend/ecommerce/src/main/java/com/poo/ecommerce/OrderItem.java
@@ -0,0 +1,18 @@
+package com.poo.ecommerce;
+
+public class OrderItem {
+    String idItem;
+    String price;
+    String quantity;
+
+    public OrderItem(String idItem2, String price2, int quantity2) {
+    }
+
+    public String getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(String idItem) {
+        this.idItem = idItem;
+    }
+}
