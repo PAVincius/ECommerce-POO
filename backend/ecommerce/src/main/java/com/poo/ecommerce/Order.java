e556a05d569696c08b0c6526e027d04a25af5ee4
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-08-16 19:48:07 -0300

    :goal_net: fix: getTotal function

--- backend/ecommerce/src/main/java/com/poo/ecommerce/Order.java
+++ backend/ecommerce/src/main/java/com/poo/ecommerce/Order.java
@@ -1,23 +1,38 @@
 package com.poo.ecommerce;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Order{
     Cpf cpf;
-    ArrayList<OrderItem> orderItem;
+    Date issueDate;
+    private ArrayList<OrderItem> orderItems;
+    Coupon coupon; //can be undefined
 
-    public Order(String cpf) {
+    public Order(String cpf, Date issueDate) {
+        this.issueDate = issueDate;
         this.cpf = new Cpf(cpf);
-        ArrayList<OrderItem> orderItems = new ArrayList<>();
-        this.orderItem = orderItems;
-
-    }
-
-    public int getTotal(){
-        return 0;
+        this.orderItems = new ArrayList<>();
     }
 
     public void addItem(Item item, int quantity){
-        this.orderItem.add(new OrderItem(item.idItem,item.price, quantity));
+        this.orderItems.add(new OrderItem(item.idItem,item.price, quantity));
     }
-    
+
+    public void addCoupon(Coupon coupon){
+        if(!coupon.isExpired()){
+            this.coupon = coupon;
+        } 
+    }
+
+    public int getTotal(){
+        /*int total = 0;
+        for (int i = 0; i < orderItems.size(); i++) {
+            total += orderItems.getTotalOrder();
+            total = 0;
+        }
+        if(coupon != null){
+            total += this.coupon.calculateDiscount(total);
+        }*/
+        return 0; //just until now
+    }   
 }
