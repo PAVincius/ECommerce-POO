56998f1cd3987a14ccd4eb5a1b4469d503c70002
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-08-16 19:41:25 -0300

    :sparkles: feat: getVolume and getDensity implementation

--- backend/ecommerce/src/main/java/com/poo/ecommerce/Item.java
+++ backend/ecommerce/src/main/java/com/poo/ecommerce/Item.java
@@ -1,11 +1,31 @@
 package com.poo.ecommerce;
 
 public class Item {
-    public Item(int i, String string, String string2, int j) {
-    }
-    String idItem;
+
+    Dimension dimension;
+    
+    int idItem;
     String category;
     String description;
-    String price;
-    
+    int price;
+    int width;
+    int height;
+    int lenght;
+
+    private double weight;
+
+    public Item(int idItem, String  category, String description, int price, Dimension dimension, int weight) {
+    }
+
+    public double getVolume(){
+        if (this.dimension != null) return this.dimension.getVolume();
+		return 0;
+    }
+    public double getDensity() {
+        if (this.weight && this.dimension != null){
+			return this.weight / this.dimension.getVolume();
+		} else {
+			return 0.0;
+		}
+    }
 }
