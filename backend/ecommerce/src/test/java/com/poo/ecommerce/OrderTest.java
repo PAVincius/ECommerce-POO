90ed7e004f496ada083d6b406fe9b62bbafc6bb0
pavincius <30844451+PAVincius@users.noreply.github.com>
2022-08-16 19:46:28 -0300

    :test_tube: test: Adding cupom unit test to order

--- backend/ecommerce/src/test/java/com/poo/ecommerce/OrderTest.java
+++ backend/ecommerce/src/test/java/com/poo/ecommerce/OrderTest.java
@@ -1,5 +1,8 @@
 package com.poo.ecommerce;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,17 +11,40 @@ public class OrderTest {
 
     @Test
     void shouldCreateOrder () {
-        Order order = new Order("73634135417");
+        Order order = new Order("73634135417", new Date());
         assertEquals(0,order.getTotal());
     }
 
     @Test
     void shouldCreateOrderWith3Items () {
-        Order order = new Order("73634135417");
+        Order order = new Order("73634135417", new Date(1661179271));
         order.addItem(new Item(1, "Notebook", "Asus Zephyrus G14", 10000),1);
         order.addItem(new Item(2, "GPU", "RTX 3090 Zotac", 8999),1);
         order.addItem(new Item(3, "Memoria RAM", "G. Skill trident z royal series", 4800),2);
         int total = order.getTotal();
+        assertEquals(28599, total);
+    }
+    
+    @Test
+    void shouldCreateOrderWith3ItemsAndCupom () {
+        Order order = new Order("73634135417", new Date(1661179271));
+        order.addItem(new Item(1, "Notebook", "Asus Zephyrus G14", 10000),1);
+        order.addItem(new Item(2, "GPU", "RTX 3090 Zotac", 8999),1);
+        order.addItem(new Item(3, "Memoria RAM", "G. Skill trident z royal series", 4800),2);
+        Coupon coupon = new Coupon("UPGRADE15", 15);
+        order.addCoupon(coupon);
+        double total = order.getTotal();
+        assertEquals(22879.2, total);
+    }
+    @Test
+    void shouldCreateOrderWith3ItemsAndExpiredCupom () {
+        Order order = new Order("73634135417", new Date(1659364871));
+        order.addItem(new Item(1, "Notebook", "Asus Zephyrus G14", 10000),1);
+        order.addItem(new Item(2, "GPU", "RTX 3090 Zotac", 8999),1);
+        order.addItem(new Item(3, "Memoria RAM", "G. Skill trident z royal series", 4800),2);
+        Coupon coupon = new Coupon("UPGRADE15", 15);
+        order.addCoupon(coupon);
+        double total = order.getTotal();
         assertEquals(28599, total);
     }
 
